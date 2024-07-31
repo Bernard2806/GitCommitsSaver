090014526ada914075d7384fd9fdf506a160401d
Bernardo <bgonzalezerramuspe@eest1chivilcoy.edu.ar>
2024-07-31 08:23:12 -0300

    Renamed project from ObtenerCommits to GitCommitsSaver

--- ObtenerCommits/MenuEasy.cs
+++ ObtenerCommits/MenuEasy.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ObtenerCommits
+namespace GitCommitsSaver
 {
     public static class MenuEasy
     {
--- ObtenerCommits/Program.cs
+++ ObtenerCommits/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using ObtenerCommits;
+using GitCommitsSaver;
 
 
 class Program
